71c1388ab3c0105ecf99f740391fd13b17ba6c7f
rishabh1244 <rishabh1244@gmail.com>
2026-07-04 15:25:17 +0530

    name fix

--- backende/dist/routes/admin/requireHrOrAdmin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requireHrOrAdmin = requireHrOrAdmin;
-function requireHrOrAdmin(req, res, next) {
-    if (typeof req.user === "string" || !req.user || !["hr", "admin"].includes(req.user.role)) {
-        return res.status(403).json({ message: "Only HR or admin users can access this route" });
-    }
-    return next();
-}
